546d7bd2906a95713d4e4fd1aa8699876aa1329d
Liran Alboher <liran.alb@gmail.com>
2019-12-15 17:32:17 +0200

    initiate -p and -r

--- client.c
+++ client.c
@@ -8,11 +8,11 @@
 
 //---------Struct for storing the command arguments ---------
 typedef struct{ // CHECK ABOUT ERRORS WHEN CHANGING THE LINES
-    char* host; // "www...."
     int port;
+    int post;
+    int arg_count;
+    char* host; // "www...."
     char* path;
-    char* command; //"GET" or "POST"
-    int pam_count;
     char* text;
 }command_t;
 //------------------------------------------------------------
@@ -20,40 +20,40 @@ typedef struct{ // CHECK ABOUT ERRORS WHEN CHANGING THE LINES
 int urlOrganizer(char *url, command_t **cmd){
     printf("\n Full temp-URL: %s\n", url);
     const char portMarker = ':';
-	//add http:// checker/ check if it's the right place for it
+    //add http:// checker/ check if it's the right place for it
     if(strchr(url+7, portMarker) == NULL) { //if we didn't find the marker ':', set the port to 80
-		char *urlTmp = strtok(url, "/;");
-		urlTmp = strtok(NULL, "/;");
-		//printf("\n Host temp is: %s", urlTmp);
-		(*cmd)->host = urlTmp;
-		//printf("\n Host at struct is: %s", (*cmd)->host);
-		urlTmp = strtok(NULL, "");
-		
-		//printf("\n path in temp is: %s", urlTmp);
-		(*cmd)->path = urlTmp;
-		//printf("\n path in struct is: %s", (*cmd)->path);
-		(*cmd)->port = 80;
-		//printf("\n port in struct is: %d", (*cmd)->port);
-		
+        char *urlTmp = strtok(url, "/;");
+        urlTmp = strtok(NULL, "/;");
+        //printf("\n Host temp is: %s", urlTmp);
+        (*cmd)->host = urlTmp;
+        //printf("\n Host at struct is: %s", (*cmd)->host);
+        urlTmp = strtok(NULL, "");
+
+        //printf("\n path in temp is: %s", urlTmp);
+        (*cmd)->path = urlTmp;
+        //printf("\n path in struct is: %s", (*cmd)->path);
+        (*cmd)->port = 80;
+        //printf("\n port in struct is: %d", (*cmd)->port);
+
     }
 
     else { //the is a port
-		char *urlTmp = strtok(url, "/;");
-		urlTmp = strtok(NULL, "/;");
-		
-		(*cmd)->path = strtok(NULL, ":");
-		//printf("\n Host temp is: %s", urlTmp);
-		//printf("\n path in struct is: %s", (*cmd)->path);
-		(*cmd)->host = strtok(urlTmp, ":/");
-		urlTmp = strtok(NULL, ":/"); //port
-		(*cmd)->port = atoi(urlTmp);
-		if(((*cmd)->port) < 0) {
-			printf("/nusage error no port");
-			exit(1);
-		}
+        char *urlTmp = strtok(url, "/;");
+        urlTmp = strtok(NULL, "/;");
+
+        (*cmd)->path = strtok(NULL, ":");
+        //printf("\n Host temp is: %s", urlTmp);
+        //printf("\n path in struct is: %s", (*cmd)->path);
+        (*cmd)->host = strtok(urlTmp, ":/");
+        urlTmp = strtok(NULL, ":/"); //port
+        (*cmd)->port = atoi(urlTmp);
+        if(((*cmd)->port) < 0) {
+            printf("/nusage error no port");
+            exit(1);
+        }
     }
 
-    if( (*cmd)->path == NULL) // define the root as a path in case no path given
+    if((*cmd)->path == NULL) // define the root as a path in case no path given
         (*cmd)->path = (char *) '/'; //check if working
 
     return 0;
@@ -61,7 +61,7 @@ int urlOrganizer(char *url, command_t **cmd){
 
 
 void parseCommand(int argc, char **argv){
-    command_t *command = (command_t*)malloc(sizeof(command)); //will be use to store the command data and arguments
+    command_t *command = (command_t*)malloc(sizeof(command_t)); //will be use to store the command data and arguments
     if(!command){
         printf("cannot allocate initiate memory for command\n");
         exit(1); //check the exit code
@@ -69,12 +69,27 @@ void parseCommand(int argc, char **argv){
 
     int i;
     for(i = 1 ; i < argc ; i++){ //going over argv
+		if((strcmp("-p", argv[i]) == 0)) {
+            if(i == argc-1){
+				printf("\nerror usage\n");
+				exit(1);
+			}
+            
+            command->post = 1;
+            printf("The post value is: %d", command->post);
+            //printf("\n arg[i+1] is: %s\n", argv[i+1]);
+            command->text = argv[i+1];
+            printf("\n struct text is: %s\n", command->text);
+        }
+        
         //       printf("\n #arv[%d] is: %s\n", i,  argv[i]);
         if((strncmp("http://", argv[i], 7) == 0)) { //url has been found
             //printf("\n #http is: %s\n",  argv[i]);
             //printf("\n #cut http: %s\n",  argv[i]+7);
-            if(urlOrganizer(argv[i], &(command)) == -1)
+            if(urlOrganizer(argv[i], &(command)) == -1){
+                free(command);
                 exit(1);
+			}
         }
 
         else { //CHECK THIS CONDITION = URL NOT FOUND
@@ -85,23 +100,26 @@ void parseCommand(int argc, char **argv){
             }
         }
 
-        if((strncmp("-p", argv[i], 7) == 0)) {
-            printf("inside http://");
-        }
 
-        if((strncmp("ir", argv[i], 7) == 0)) {
-            printf("inside http://");
+        if((strcmp("-r", argv[i]) == 0)) { //change to strcmp later
+            if(i == argc-1){
+				printf("\nerror usage\n");
+				exit(1);
+			}
+			printf("/m type of arg counter is is %s", argv[i+1]);
+			
         }
 
 
     }
-	printf("********************************\n");
+    printf("********************************\n");
     printf("*** Struct Checker From Main ***\n");
     printf("********************************\n");
     printf("The host is: %s\n", command->host);
     printf("The Path is: %s\n", command->path);
     printf("The port is: %d", command->port);
-    
+   
+
     free(command);
 }
 
@@ -114,6 +132,7 @@ int main(int argc,char *argv[]) {
         printf("%s\n" ,argv[i]);
     }
 */
+	
     parseCommand(argc, argv);
     return 0;
 }
