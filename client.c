d32b2a19c6e621f8fef55baf3c897c6d11c9d1f2
Liran Alboher <liran.alb@gmail.com>
2019-12-23 17:41:35 +0200

    add errors check

--- client.c
+++ client.c
@@ -34,7 +34,7 @@ void reqBuilder(command_t **cmd);
 void usageERR(command_t **cmd);
 void freeAlloc(command_t *cmd);
 
-/* in  there is -r, this method will check if the arguments are valid
+/* in case there is -r, this method will check if the arguments are valid
  * the method will return the current i after checking all the arguments, or
  * -1 if it's fails. This method also creates a string contains the arguments in
  * the right format inside the command struct.*/
@@ -96,8 +96,6 @@ int request(command_t **cmd, char **argv, int argc, int *i){
             strcat(tmpText, "&");
     }
 
-    //printf("\nThe temp text is %s\n", tmpText);
-
     ((*cmd)->arguStr) = (char*)malloc(sizeof(char) * (strlen(tmpText)+1));
     if(!((*cmd)->arguStr)) {
         printf("Cannot allocate initial memory for data\n");
@@ -105,8 +103,6 @@ int request(command_t **cmd, char **argv, int argc, int *i){
     }
 
     strcpy(((*cmd)->arguStr), tmpText);
-
-    //printf("\nThe struct text is %s\n", (*cmd)->arguStr);
 
     if(count == argCount){ //return the index of the first cell that comes after the arguments
         (*i)=(*i)-1;
@@ -123,13 +119,10 @@ int urlOrganizer(char *url, command_t **cmd){
     const char portMarker = ':';
     if(strchr(url+7, portMarker) == NULL) { //if we didn't find the marker ':', set the port to 80
         char *urlTmp = strtok(url, "/");
-        //printf("\nURL cut check: %s\n", urlTmp);
         urlTmp = strtok(NULL, "/");
-        //printf("\nURL cut check: %s\n", urlTmp);
         (*cmd)->host = urlTmp;
         urlTmp = strtok(NULL, "");
-        //printf("\nURL cut check: %s\n", urlTmp);
-        printf("\nurlTmp: %s\n", urlTmp);
+        //printf("\nurlTmp: %s\n", urlTmp);
         if(urlTmp == NULL) { // define the root as a path in case no path given
             //check if working
             ((*cmd)->path) = (char*)malloc(sizeof(char)*2);
@@ -138,14 +131,12 @@ int urlOrganizer(char *url, command_t **cmd){
             strcpy(((*cmd)->path), "/");
         }
         else {
-            printf("\nThe path is:%s\n", urlTmp);
             strcat(urlTmp, "\0");
             ((*cmd)->path) = (char*)malloc(sizeof(char)*strlen(urlTmp)+1);
             if(!(*cmd)->path)
                 return -1;
             strcat((*cmd)->path, "/");
             strcat((*cmd)->path, urlTmp);
-            printf("\nThe tmp path is:%s\n", (*cmd)->path);
 
             //printf("\nThe path is:%s\n", t);
 
@@ -158,7 +149,6 @@ int urlOrganizer(char *url, command_t **cmd){
     else { //there is a port
         char *urlTmp = strtok(url, "/");
         urlTmp = strtok(NULL, "/");
-
         (*cmd)->path = strtok(NULL, ":");
         (*cmd)->host = strtok(urlTmp, ":/");
         urlTmp = strtok(NULL, ":/"); //port
@@ -176,28 +166,23 @@ int urlOrganizer(char *url, command_t **cmd){
 
 void reqBuilder(command_t **cmd) {
     char tmp[256];
-    if((*cmd)->post == 0){
+    if((*cmd)->post <= 0){
         strcpy(tmp, "GET ");
 
     }
     else
         strcpy(tmp, "POST ");
-    //printf("\nThe temp req is %s\n", tmp);
-    //printf("The Path is: %s\n", (*cmd)->path);
 
     if(((*cmd)->path)){
         strcat(tmp, (*cmd)->path);
     }
 
-    //printf("\nThe temp req is %s\n", tmp);
     if((*cmd)->arguStr){
         strcat(tmp, (*cmd)->arguStr);
-        //free(((*cmd)->arguStr));
     }
-    //printf("\nThe temp req is %s\n", tmp);
+
     strcat(tmp, " ");
     strcat(tmp, PROTOCOL);
-    //printf("\nThe temp req is %s\n", tmp);
     strcat(tmp, "\r\nHOST: ");
     strcat(tmp, (*cmd)->host);
     strcat(tmp, "\r\n");
@@ -206,29 +191,23 @@ void reqBuilder(command_t **cmd) {
         (*cmd)->strRequest = (char*)malloc(sizeof(char)*(strlen(tmp)+1));
         if(!((*cmd)->strRequest)) {
             printf("Cannot allocate initial memory for data\n");
-            //freeAlloc(cmd);
+            freeAlloc(*cmd);
             exit(1);
         }
         strcpy((*cmd)->strRequest, tmp);
     }
 
 
-    if((*cmd)->post == 1){
+    if((*cmd)->post > 0){
         strcat(tmp, "Content-length:");
 
         char buffer[10];
-        //memset(&buffer, 0, sizeof(buffer));
         int value = (int)strlen((*cmd)->text);
-        //printf("\n#######Value check: %d#######", value);
-        //printf("\nvalue is: %d",value);
         sprintf(buffer, "%d", value);
-        //printf("\nBuffer is: %s\n", buffer);
         strcat(tmp, buffer);
         strcat(tmp, "\r\n\r\n");
-        //printf("\n the text is: %s", (*cmd)->text);
         strcat(tmp, (*cmd)->text);
         strcat(tmp, "\0");
-        //printf("\nThe post is:%s\n", tmp);
         (*cmd)->strRequest = (char*)malloc(sizeof(char)*(strlen(tmp)+1));
         if(!((*cmd)->strRequest)) {
             printf("Cannot allocate initial memory for data\n");
@@ -236,7 +215,6 @@ void reqBuilder(command_t **cmd) {
             exit(1);
         }
         strcpy((*cmd)->strRequest, tmp);
-        //printf("\nThe req is:%s\n", (*cmd)->strRequest);
     }
 }
 
@@ -287,7 +265,6 @@ int main(int argc,char *argv[]) {
             }
 
             strcpy(command->text, argv[i+1]);
-            //printf("\n The p text is %s", command->text);
             i++; //skip the text for the next checks
             continue;
         }
@@ -308,7 +285,7 @@ int main(int argc,char *argv[]) {
             }
 
             if((strcmp("0", argv[i+1]) == 0)){ //check if there is no arguments
-                command->post = 0;
+                //command->post = 0;
                 i++;
                 continue;
             }
@@ -324,7 +301,6 @@ int main(int argc,char *argv[]) {
                 usageERR(&command);
                 exit(1);
             }
-            command->post = 0;
             continue;
 
 
@@ -336,22 +312,7 @@ int main(int argc,char *argv[]) {
     }
 
     reqBuilder(&command);
-
-
-    printf("********************************\n");
-    printf("*** Struct Checker From Main ***\n");
-    printf("********************************\n");
-    printf("The host is: %s\n", command->host);
-    printf("The Path is: %s\n", command->path);
-    printf("The port is: %d\n", command->port);
-    printf("The arg's are: %s\n", command->arguStr);
-    printf("The post text is %s\n", command->text);
-    printf("The post text is %s\n", command->text);
-    printf("The post text is %s\n", command->text);
-    printf("The post text is %s\n", command->text);
-    printf("The request is\n %s\n", command->strRequest);
-
-
+    printf("HTTP request=\n%s\nLEN=%d\n", command->strRequest, strlen(command->strRequest));
     int sockfd; //return file descriptor when succeed;
 
     int rc;
@@ -360,33 +321,19 @@ int main(int argc,char *argv[]) {
     struct  hostent *server;
     char rbuf[BUFLEN];
     char* buffer = command->strRequest;
-    //char * buffer = "POST /?addr=jerusalem&tel=02-6655443&age=23 HTTP/1.0\r\nHOST:www.ptsv2.com\nContent-length:6\r\n\r\nblablmnmjkmma";
-    //printf("\nBUFFER CHECK: %s", buffer);
-    //"./%e" -r 3 addr=jecrusalem tel=02-6655443 age=23 http://www.ptsv2.com/t/ex2
+
     if(argc < 3){
         printf(USAGE_ERR);
         exit(0);
     }
 
-    //server = gethostbyname(command.host);
-    //printf("\nServer name is: %s" ,server->h_name);
-
-
-
 //---------------------------- connection--------------------------------------------------
 
     sockfd = socket( AF_INET, SOCK_STREAM , 0); //create an endpoint for communication
-
-
-
-
     if(sockfd < 0){ //checking id succeed
-        printf("error opening socket");
+        perror("socket");
         exit(0);
     }
-
-
-    //printf("\nserver name is:#%s#\n" ,command->host);
 
     server = gethostbyname(command->host);
     //printf("\nserver after is:#%s#\n" , server->h_name);
@@ -401,10 +348,16 @@ int main(int argc,char *argv[]) {
     serv_addr.sin_port = htons(command->port);
 
     //initiate connection on a socket
-    if(connect(sockfd, (const struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) //initiate connection on a socket
+    if(connect(sockfd, (const struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) { //initiate connection on a socket
         perror("ERROR connecting");
+        exit(1);
+    }
 
     rc = write(sockfd, buffer, strlen(buffer)+1);
+    if(rc < 0){
+        perror("write");
+        exit(1);
+    }
 
     while(1){
         if(rc == 0)
@@ -413,7 +366,6 @@ int main(int argc,char *argv[]) {
 
         rbuf[rc] = '\0';
         printf("%s", rbuf);
-        //bzero(rbuf,sizeof(rbuf));
     }
 
     close(sockfd);
